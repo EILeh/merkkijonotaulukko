bea41bb59e146cd73a80ad7fce9de04c22fed11b
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-11-23 15:50:14 +0200

    for loopin avulla kopioidaan annettu taulukko uuteen muistiin ja palautetaan uusi taulukko

--- mjonot.c
+++ mjonot.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 #include "mjonot.h"
 
 char ** kopioi_mjt(char **mjt, size_t lkm)
 {
+    char **x = malloc(lkm*sizeof(char *));
+    int i = 0;
+    char **y;
+
+    for (i = 0; i < lkm ; ++i)
+    {
+        x[i] = malloc((strlen(mjt[i]) + 1) * sizeof(char));
+
+        strcpy(x[i], mjt[i]);
+    }
+    return x;
+
 
 
 }
